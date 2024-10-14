9a81594f4914a85e37b10e51c99ce1010962f886
ryandwren <ryandwren@gmail.com>
2024-10-14 14:27:58 -0400

    Extra feature color coded list

--- app/src/main/java/com/ryandwren/fetchdemoapp/ui/MainScreen.kt
+++ app/src/main/java/com/ryandwren/fetchdemoapp/ui/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.ryandwren.fetchdemoapp.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ryandwren.fetchdemoapp.data.HireItem
@@ -61,8 +63,17 @@ fun MainScreen(state: MainScreenState, modifier: Modifier) {
 
 @Composable
 fun ItemCard(item: HireItem) {
+    var backgroundColor = Color.White
+    when(item.listId){
+        1 -> backgroundColor = Color(0x5F6298f0)
+        2 -> backgroundColor = Color(0x5Ff0e278)
+        3 -> backgroundColor = Color(0x5Fff6961)
+        4 -> backgroundColor = Color(0x5F88ff7d)
+    }
+
     Row(
         modifier = Modifier
+            .background(backgroundColor)
             .padding(horizontal = 24.dp, vertical = 8.dp)
             .fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween
@@ -80,14 +91,14 @@ fun MainScreenPreview() {
         MainScreen(
             state = MainScreenState().apply {
                 hireList.value = listOf(
-                    HireItem(1, 1, "Item 1"),
-                    HireItem(1, 2, "Item 2"),
-                    HireItem(1, 3, "Item 3"),
-                    HireItem(1, 4, "Item 4"),
-                    HireItem(2, 1, "Item 1"),
-                    HireItem(2, 2, "Item 2"),
-                    HireItem(2, 3, "Item 3"),
-                    HireItem(2, 4, "Item 4"),
+                    HireItem(1,1,"Item 1"),
+                    HireItem(2,1,"Item 2"),
+                    HireItem(3,1,"Item 3"),
+                    HireItem(4,1,"Item 4"),
+                    HireItem(1,2,"Item 1"),
+                    HireItem(2,2,"Item 2"),
+                    HireItem(3,2,"Item 3"),
+                    HireItem(4,2,"Item 4"),
                 )
             },
             modifier = Modifier
