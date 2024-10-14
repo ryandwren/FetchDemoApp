3522dee764903e2c9e88702300629392708e7959
ryandwren <ryandwren@gmail.com>
2024-10-14 10:52:43 -0400

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,13 @@ android {
 
 dependencies {
 
+    implementation(libs.dagger.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(libs.retrofit)
+
+    implementation(libs.gson)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
